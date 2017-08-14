45936d529969ee3846aafc7e5a53d404d953d924
showtime509 <superliu622@gmail.com>
2017-08-14 14:47:24 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const PORT = 8080;
 const app = express();
 app.get('/', function (req, res) {
   res.send('Hello world\n');
-});ls
+});
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
